2afe659c998ed4eb44c0db9f4c3b27112fccf916
Valentyn <J.K.play2015@gmail.com>
2024-07-29 18:54:01 +0300

    create project

---
+++ src/main/java/com/example/buildart/BuildArtApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildArtApplication.class, args);
+	}
+
+}

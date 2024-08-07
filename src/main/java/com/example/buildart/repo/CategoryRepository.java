c00c3789416eabe6b7d9ec634a09c96c2a97760f
Valentyn <J.K.play2015@gmail.com>
2024-08-07 20:09:47 +0300

    create repository of category

---
+++ src/main/java/com/example/buildart/repo/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.buildart.repo;
+
+import com.example.buildart.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}

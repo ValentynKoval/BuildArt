d7fd2dcabf42090df4edf56a6d30be535b5fec29
Valentyn <J.K.play2015@gmail.com>
2024-08-07 20:43:44 +0300

    create dto of category

---
+++ src/main/java/com/example/buildart/dto/CategoryDto.java
@@ -0,0 +1,13 @@
+package com.example.buildart.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+    private String name;
+    private String description;
+}

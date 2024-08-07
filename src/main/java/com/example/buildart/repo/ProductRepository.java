2972623f5acb6f38555bc11ba5e013aa5ec6d33d
Valentyn <J.K.play2015@gmail.com>
2024-08-07 20:10:05 +0300

    create repository of product

---
+++ src/main/java/com/example/buildart/repo/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.buildart.repo;
+
+import com.example.buildart.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
